7f2adc9e2342fd1e675c6c50248f85e7042b85a4
odeyalo <theprogrammerinfuture@gmail.com>
2025-12-19 20:20:45 +0200

    Add endpoints to fetch all and by ID animes

--- src/main/java/com/odeyalo/animerestapi/entity/Anime.java
+++ src/main/java/com/odeyalo/animerestapi/entity/Anime.java
@@ -1,20 +1,43 @@
 package com.odeyalo.animerestapi.entity;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Data;
+import lombok.*;
 import lombok.experimental.FieldDefaults;
+import org.hibernate.proxy.HibernateProxy;
+import org.jspecify.annotations.Nullable;
+
+import java.util.Objects;
 
 @Entity
 @Table(name = "animes")
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class Anime {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Nullable
     Integer id;
     @Column(name = "name", nullable = false)
     String name;
     @Column(name = "description")
     String description;
+
+    @Override
+    public final boolean equals(final Object o) {
+        if ( this == o ) return true;
+        if ( o == null ) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if ( thisEffectiveClass != oEffectiveClass ) return false;
+        final Anime anime = (Anime) o;
+        return getId() != null && Objects.equals(getId(), anime.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }
--- src/main/java/com/odeyalo/animerestapi/repository/AnimeRepository.java
+++ src/main/java/com/odeyalo/animerestapi/repository/AnimeRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AnimeRepository extends JpaRepository<Anime, Long> {
+public interface AnimeRepository extends JpaRepository<Anime, Integer> {
 }
